c3aac3a85de3d574fbb14c53d7804dc64ecf456f
Travis Gandy <travis_gandy@yahoo.com>
2020-03-15 15:25:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,13 +7,11 @@ def my_collect(x)
 end
 
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
 my_collect(array) do |name|
   name.split(/ /).first
 end
 
-collection = ['ruby', 'javascript', 'python', 'objective-c']
+
 my_collect(collection) do |lang|
   lang.upcase
 end
