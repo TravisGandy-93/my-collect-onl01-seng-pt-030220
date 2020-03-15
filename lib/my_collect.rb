7002351ca0b6bdffe1cc4f19ee90470f3ce06c3b
Travis Gandy <travis_gandy@yahoo.com>
2020-03-15 15:56:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+
+def my_collect(x)
+  new_array = []
+  i = 0
+  while i < x.size
+  new_array << yield(x[i])
+    i += 1
+  end
+  new_array
+end
 
 
