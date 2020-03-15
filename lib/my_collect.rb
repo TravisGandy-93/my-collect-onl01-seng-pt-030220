db87a70cf25e50210d6afeffa9e13e19c21ed6bb
Travis Gandy <travis_gandy@yahoo.com>
2020-03-15 15:37:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,9 @@ collection = ['ruby', 'javascript', 'python', 'objective-c']
 def my_collect(x)
   i = 0
   yield(x)
-  while x.size > 0 
+  while x.size < 10 
+    i += 1
+  end
   puts "we're collecting now."
 end
 
